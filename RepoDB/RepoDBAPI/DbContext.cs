8b137eb620f1e7c326fdcb4054f858b94c559c50
MH.Yazdani <mohaymen@YAZDANI-PC>
2025-11-19 14:36:20 +0330

    fix the project and ready

--- RepoDB/RepoDBAPI/DbContext.cs
+++ RepoDB/RepoDBAPI/DbContext.cs
@@ -1,15 +1,17 @@
-﻿using RepoDb;
-using System.Data.SQLite;
+﻿using Microsoft.Data.Sqlite;
 
 namespace RepoDBAPI
 {
     public class DbContext : IDisposable
     {
         private readonly string _connectionString;
-        public DbContext(IConfiguration configuratioin) => _connectionString = configuratioin.GetConnectionString("SqliteConnection");
+        public DbContext(IConfiguration configuratioin)
+        {
+            _connectionString = configuratioin.GetConnectionString("SqliteConnection");
+        }
 
-        public SQLiteConnection AppDbContext
-            => new SQLiteConnection(_connectionString);
+        public SqliteConnection AppDbContext
+            => new SqliteConnection(_connectionString);
 
         public void Dispose()
         {
--- RepoDB/RepoDBAPI/Program.cs
+++ RepoDB/RepoDBAPI/Program.cs
@@ -1,8 +1,10 @@
 using RepoDb;
 using RepoDBAPI;
+using Scalar.AspNetCore;
 
 public partial class Program
 {
+    [Obsolete]
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +14,7 @@ public partial class Program
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
-        SQLiteBootstrap.Initialize();
+        SqliteBootstrap.Initialize();
         IConfiguration config = builder.Configuration;
         var context = new DbContext(config);
         using var dataBase = context.AppDbContext;
@@ -22,14 +24,25 @@ public partial class Program
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            app.MapScalarApiReference(options =>
+            {
+                options.WithTitle("My API");
+                options.WithOpenApiRoutePattern("/openapi/v1.json");
+            });
         }
-        app.MapPost("/Insert", async (string x) => {
+        app.MapPost("/Insert", async (User x) => {
             var a = await dataBase.InsertAsync(x);
             return a.ToString();
         });
 
-        app.MapPost("/Get", async (int id) => {
-            var a = (await dataBase.QueryAsync<User>(id));
+        app.MapGet("/Get/{id}", async (int id) => {
+            var a = (await dataBase.QueryAsync<User>(id)).FirstOrDefault();
+            return a;
+        });
+
+        app.MapDelete("/Delete/{id}", async (int id) =>
+        {
+            var a = await dataBase.DeleteAsync<User>(id);
             return a.ToString();
         });
 
